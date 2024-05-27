4188d24ac5de85289765f3ec647f557e595e5da7
vethanathan <you@example.com>
2024-05-27 23:05:50 +0530

    Added move validator for Tiger user

--- Boards.py
+++ Boards.py
@@ -1,17 +1,26 @@
 # from Validator import Validator
+def find_coordinate_between(index1, index2):
+        # Calculate the average of each dimension to find the midpoint
+        x_coord = (index1[0] + index2[0]) / 2
+        y_coord = (index1[1] + index2[1]) / 2
+        if (int(x_coord),int(y_coord))  in (index1,index2):
+            return False
+        return (int(x_coord), int(y_coord))  # Return as integers
+
+
 class Board:
-    board_vals= {'a': (0, 2), 'b': (1, 0), 'c': (1, 1), 'd': (1, 2), 'e': (1, 3), 'f': (1, 4), 'g': (1, 5), 'h': (2, 0), 'i': (2, 1), 'j': (2, 2), 'k': (2, 3), 'l': (2, 4), 'm': (2, 5), 'n': (3, 0), 'o': (3, 1), 'p': (3, 2), 'q': (3, 3), 'r': (3, 4), 's': (3, 5), 't': (4, 1), 'u': (4, 2), 'v': (4, 3), 'w': (4, 4)}
+    #board_vals= {'a': (0, 2), 'b': (1, 0), 'c': (1, 1), 'd': (1, 2), 'e': (1, 3), 'f': (1, 4), 'g': (1, 5), 'h': (2, 0), 'i': (2, 1), 'j': (2, 2), 'k': (2, 3), 'l': (2, 4), 'm': (2, 5), 'n': (3, 0), 'o': (3, 1), 'p': (3, 2), 'q': (3, 3), 'r': (3, 4), 's': (3, 5), 't': (4, 1), 'u': (4, 2), 'v': (4, 3), 'w': (4, 4)}
     invalid_dest=[(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (4, 0), (4, 0)]
     
 
     def __init__(self):
 
         self.board =[
-            ['#','#','a','#','#','#'],
-            ['b','c','d','e','f','g'],
-            ['h','i','j','k','l','m'],
-            ['n','o','p','q','r','s'],
-            ['#','t','u','v','w','#']
+            ['#','#','_','#','#','#'],
+            ['_','_','_','_','_','_'],
+            ['_','_','_','_','_','_'],
+            ['_','_','_','_','_','_'],
+            ['#','_','_','_','_','#']
             ] 
         self.goat = 15
         self.tiger = 3
@@ -21,11 +30,33 @@ class Board:
             for j in range(6):
                 print(self.board[i][j], end=" ")
             print()
-    def make_move(self,from_board,to_board):
+    def make_move(self,from_board,to_board,role):
+        from_row, from_col = from_board
+        to_row, to_col = to_board
         if not self.isvalid_move(from_board,to_board):
             raise Exception("Invalid Move")
-        else:   
-            pass
+        else:
+            if role == 'tiger':
+                if not from_board == ('*','*'):
+                    tup = find_coordinate_between(from_board,to_board)
+                if from_board==('*','*'):
+                    if self.board[to_row][to_col] == 'G':
+                        raise Exception("Invalid Move")
+                    elif self.tiger!=0 and self.board[to_row][to_col]=='_':
+                        self.board[to_row][to_col] = 'T'
+                else:
+                    if tup:
+                        if self.board[from_row][to_col] == 'T':
+                            self.goat -= 1
+                            self.board[tup[0]][tup[1]] = '_'
+                            self.board[from_row][from_col],self.board[to_row][to_col] = self.board[to_row][to_col],self.board[from_row][from_col]
+                        else:
+                            raise Exception("Invalid Move")
+                    else:
+                        self.board[from_row][from_col],self.board[to_row][to_col] = self.board[to_row][to_col],self.board[from_row][from_col]
+                
+
+            
 
         
     def board_index(self):
@@ -33,6 +64,13 @@ class Board:
         invalid_dest=[(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (4, 0), (4, 0)]
 
     def isvalid_move(self,from_,to_):
+        if from_ ==('*','*') and self.board[to_[0]][to_[1]] =='_':
+            return True
+    
+        if from_ ==('*','*') and self.board[to_[0]][to_[1]] =='T':
+            return False
+        if from_ ==('*','*') and self.board[to_[0]][to_[1]] =='G':
+            return False
 
         from_row, from_col = from_
         to_row, to_col = to_
@@ -66,3 +104,12 @@ class Board:
     
 
 
+b=Board()
+b.print_board()
+b.make_move(('*','*'),(0,2),'tiger')
+b.print_board()
+b.make_move((1,2),(3,2),'tiger')
+b.print_board()
+
+
+print(b.tiger,b.goat)
